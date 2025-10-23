9e189c7af226c7988fc8126829234aaec9ae8af9
ArdisKallja <ardis@example.com>
2025-10-23 14:00:35 +0200

    21.10.2025

--- Spielwiese.py
+++ Spielwiese.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 import time
 import os
+import random
 
 # Hier ist die Ablage für alle Eigenschaften und Attribute, die der Charakter hochleveln kann.
 Rot = "\033[31m"
@@ -15,5 +16,8 @@ def clean_print(text): #clean("dein Text") ruft die DEF auf // Clean ("dein Text
         sys.stdout.write(c); sys.stdout.flush(); time.sleep(0.002)
 
 
+print = """Du weißt, je länger du brauchst, die Handys aus deren jeweiligen Kisten auszupacken und an deinem Körper zu kleben, 
+        \ndesto wahrscheinlicher wirst du erwischt.
+          Du kannst entscheiden, ob du 15 Geräte auspackst[1], 8 Geräte[2],4Geräte[3] oder 2 Geräte[4].
 
-
+"""
